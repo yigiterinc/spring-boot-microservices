7a555fc43db44f7991ee93c99dc3d5490b6b641e
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2021-09-08 17:11:12 +0300

    Change RatingsResource to expect userId and return a list of ratings

--- ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratings")
 public class RatingsResource {
 
-    @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable String movieId) {
-        return new Rating(movieId, 4);
+    @RequestMapping("/{userId}")
+    public List<Rating> getRatingsOfUser(@PathVariable String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                 new Rating("5678", 3)
+        );
+
+        return ratings;
     }
 }
