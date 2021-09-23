a050253262251a5c7293e9a14fdef2e0f757f063
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2021-09-23 16:24:49 +0300

    Configure Hystrix properties, add Hystrix dashboard

--- movie-catalog-service/src/main/java/com/moviecatalogservice/services/MovieInfoService.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/services/MovieInfoService.java
@@ -4,6 +4,7 @@ import com.moviecatalogservice.models.CatalogItem;
 import com.moviecatalogservice.models.Movie;
 import com.moviecatalogservice.models.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +17,17 @@ public class MovieInfoService {
         this.restTemplate = restTemplate;
     }
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+            commandProperties = {
+                    // Time to cause timeout
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    // N, Hystrix looks at (analyzes) last N requests.
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    // if >= 50 percent of the last N requests fail, break the circuit
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    // Wait/Sleep for 5 seconds before sending another request to the failed service
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public CatalogItem getCatalogItem(Rating rating) {
         String movieDetailsUrl = "http://movie-info-service/movies/" + rating.getMovieId();
         Movie movie = this.restTemplate.getForObject(movieDetailsUrl, Movie.class);
--- movie-catalog-service/src/main/java/com/moviecatalogservice/services/UserRatingService.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/services/UserRatingService.java
@@ -3,6 +3,7 @@ package com.moviecatalogservice.services;
 import com.moviecatalogservice.models.Rating;
 import com.moviecatalogservice.models.UserRating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
@@ -19,7 +20,17 @@ public class UserRatingService {
         this.restTemplate = restTemplate;
     }
 
-    @HystrixCommand(fallbackMethod = "getFallbackUserRatings")
+    @HystrixCommand(fallbackMethod = "getFallbackUserRatings",
+        commandProperties = {
+                // Time to cause timeout
+                @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                // N, Hystrix looks at (analyzes) last N requests.
+                @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                // if >= 50 percent of the last N requests fail, break the circuit
+                @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                // Wait/Sleep for 5 seconds before sending another request to the failed service
+                @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+        })
     public UserRating getUserRating(@PathVariable("userId") String userId) {
         String ratingsUrl = "http://ratings-data-service/ratings/" + userId;
         return Objects.requireNonNull(restTemplate.getForObject(ratingsUrl, UserRating.class));
